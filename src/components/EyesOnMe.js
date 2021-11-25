091ff84c6d56ee72cc526df7d9ce39b645baaa1a
harris <khanharris7@yahoo.com>
2021-11-25 16:15:09 -0600

    commit tree e2b8f7b23e3dd1e1c42d95cc630f242a09001758

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function lookAtMe(event) {
+        console.log(event);
+    }
+    return (
+        <div>
+    <button onBlur={(event) => {
+        console.log('Hey! Eyes on me!')
+    }}
+
+    onFocus={(event) => {
+        console.log('Good!')
+    }}
+   
+    >Eyes on me</button>
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+function Keypad() {
+    function handleChange(event) {
+        console.log(`${event.target.placeholder}`);
+    }
+    return(
+        <div>
+            <input
+            type="password"
+            onChange={handleChange}
+            placeholder="Entering password..."
+
+            ></input>
+        </div>
+    )
+}
+
+export default Keypad;
